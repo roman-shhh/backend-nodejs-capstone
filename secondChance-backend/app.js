acea43f63f5d6f372ead2f774d1987041c288b7c
«roman-shhh» <roman@sheremetiev.pro>
2024-12-19 10:15:48 -0500

    Create API for SecondChanceItems Service

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -7,9 +7,12 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
+
 
 const app = express();
 app.use("*",cors());
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
