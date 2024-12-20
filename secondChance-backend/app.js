39afdf415ffe63ef3818f9aaab0008298ee62fd6
Roman Sh <55157344+roman-shhh@users.noreply.github.com>
2024-12-20 11:26:24 +0100

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -9,7 +9,7 @@ const connectToDatabase = require('./models/db')
 // const { loadData } = require("./util/import-mongo/index")
 
 const app = express()
-app.use("*",cors())
+app.use("*", cors())
 const port = 3060
 
 // Connect to MongoDB; we just do this one time
@@ -18,8 +18,7 @@ connectToDatabase().then(() => {
 })
   .catch((e) => console.error('Failed to connect to DB', e))
 
-
-app.use(express.json());
+app.use(express.json())
 
 // Route files
 const pinoHttp = require('pino-http')
@@ -34,7 +33,7 @@ const secondChanceRoutes = require('./routes/secondChanceItemsRoutes')
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes')
 
-app.use(pinoHttp({ logger }));
+app.use(pinoHttp({ logger }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Use Routes
@@ -49,12 +48,12 @@ app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-  console.error(err);
+  console.error(err)
   res.status(500).send('Internal Server Error')
 })
 
-app.get('/', (req,res)=>{
-  res.send("Inside the server")
+app.get('/', (req,res) => {
+  res.send('Inside the server')
 })
 
 app.listen(port, () => {
