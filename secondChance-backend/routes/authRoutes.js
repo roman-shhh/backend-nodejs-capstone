b95c94d55bd8ec2bc78932c7b0e0e509342a6f3a
Roman Sh <55157344+roman-shhh@users.noreply.github.com>
2024-12-20 11:40:39 +0100

    Update authRoutes.js

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,159 +1,159 @@
-const express = require('express');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const connectToDatabase = require('../models/db');
-const router = express.Router();
-const dotenv = require('dotenv');
-const pino = require('pino');  // Import Pino logger
+const express = require('express')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const connectToDatabase = require('../models/db')
+const router = express.Router()
+const dotenv = require('dotenv')
+const pino = require('pino')  // Import Pino logger
 
 //Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
 const { body, validationResult } = require('express-validator');
 
-const logger = pino();  // Create a Pino logger instance
+const logger = pino() // Create a Pino logger instance
 
 dotenv.config();
 
 //Create JWT secret
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET
 
 router.post('/register', async (req, res) => {
-    try {
-        //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-        const db = await connectToDatabase();
-        // Task 2: Access MongoDB `users` collection
-        const collection = db.collection("users");
-        const existingEmail = await collection.findOne({ email: req.body.email });
+  try {
+    //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+    // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+    const db = await connectToDatabase()
+    // Task 2: Access MongoDB `users` collection
+    const collection = db.collection('users')
+    const existingEmail = await collection.findOne({ email: req.body.email })
 
-        // Task 3: Check if user credentials already exists in the database and throw an error if they do
-        if (existingEmail) {
-            logger.error('Email id already exists');
-            return res.status(400).json({ error: 'Email id already exists' });
-        }
-
-        // Task 4: Create a hash to encrypt the password so that it is not readable in the database
-        const salt = await bcryptjs.genSalt(10);
-        const hash = await bcryptjs.hash(req.body.password, salt);
-        const email=req.body.email;
-        console.log('email is',email);
-
-        // Task 5: Insert the user into the database
-        const newUser = await collection.insertOne({
-            email: req.body.email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: hash,
-            createdAt: new Date(),
-        });
-
-        // Task 6: Create JWT authentication if passwords match with user._id as payload
-        const payload = {
-            user: {
-                id: newUser.insertedId,
-            },
-        };
-
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        // Task 7: Log the successful registration using the logger
-        logger.info('User registered successfully');
-        // Task 8: Return the user email and the token as a JSON
-        res.json({ authtoken, email });
-    } catch (e) {
-        logger.error(e);
-        return res.status(500).send('Internal server error');
+    // Task 3: Check if user credentials already exists in the database and throw an error if they do
+    if (existingEmail) {
+      logger.error('Email id already exists')
+      return res.status(400).json({ error: 'Email id already exists' })
     }
-});
+
+    // Task 4: Create a hash to encrypt the password so that it is not readable in the database
+    const salt = await bcryptjs.genSalt(10)
+    const hash = await bcryptjs.hash(req.body.password, salt)
+    const email=req.body.email
+    console.log('email is', email)
+
+    // Task 5: Insert the user into the database
+    const newUser = await collection.insertOne({
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: hash,
+      createdAt: new Date()
+    })
+
+    // Task 6: Create JWT authentication if passwords match with user._id as payload
+    const payload = {
+      user: {
+        id: newUser.insertedId
+      }
+    }
+
+    const authtoken = jwt.sign(payload, JWT_SECRET)
+    // Task 7: Log the successful registration using the logger
+    logger.info('User registered successfully')
+    // Task 8: Return the user email and the token as a JSON
+    res.json({ authtoken, email })
+  } catch (e) {
+    logger.error(e)
+    return res.status(500).send('Internal server error')
+  }
+})
 
 router.post('/login', async (req, res) => {
-    console.log("\n\n Inside login")
+  console.log("\n\n Inside login")
 
-    try {
-        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-        const db = await connectToDatabase();
-        // Task 2: Access MongoDB `users` collection
-        const collection = db.collection("users");
-        // Task 3: Check for user credentials in database
-        const theUser = await collection.findOne({ email: req.body.email });
-        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
-        if (theUser) {
-            let result = await bcryptjs.compare(req.body.password, theUser.password)
+  try {
+    // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+    const db = await connectToDatabase()
+    // Task 2: Access MongoDB `users` collection
+    const collection = db.collection('users')
+    // Task 3: Check for user credentials in database
+    const theUser = await collection.findOne({ email: req.body.email })
+    // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+    if (theUser) {
+      let result = await bcryptjs.compare(req.body.password, theUser.password)
 
-            if(!result) {
-                logger.error('Passwords do not match');
-                return res.status(404).json({ error: 'Wrong pasword' });
-            }
-            // Task 5: Fetch user details from a database
-            let payload = {
-                user: {
-                    id: theUser._id.toString(),
-                },
-            };
-
-            const userName = theUser.firstName;
-            const userEmail = theUser.email;
-            // Task 6: Create JWT authentication if passwords match with user._id as payload
-            const authtoken = jwt.sign(payload, JWT_SECRET);
-            logger.info('User logged in successfully');
-            return res.status(200).json({ authtoken, userName, userEmail });
-        }  else {
-            // Task 7: Send appropriate message if the user is not found
-            logger.error('User not found');
-            return res.status(404).json({ error: 'User not found' });
+      if(!result) {
+        logger.error('Passwords do not match')
+        return res.status(404).json({ error: 'Wrong pasword' })
+      }
+    
+      // Task 5: Fetch user details from a database
+      let payload = {
+        user: {
+            id: theUser._id.toString()
         }
-    } catch (e) {
-        logger.error(e);
-        return res.status(500).send('Internal server error');
+      }
 
+    const userName = theUser.firstName
+    const userEmail = theUser.email
+    // Task 6: Create JWT authentication if passwords match with user._id as payload
+    const authtoken = jwt.sign(payload, JWT_SECRET)
+    logger.info('User logged in successfully')
+    return res.status(200).json({ authtoken, userName, userEmail })
+    }  else {
+      // Task 7: Send appropriate message if the user is not found
+      logger.error('User not found')
+      return res.status(404).json({ error: 'User not found' })
     }
-});
+  } catch (e) {
+    logger.error(e)
+    return res.status(500).send('Internal server error')
+
+  }
+})
 
 router.put('/update', async (req, res) => {
-    // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
-    const errors = validationResult(req);
+  // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
+  const errors = validationResult(req)
 
-    if (!errors.isEmpty()) {
-        logger.error('Validation errors in update request', errors.array());
-        return res.status(400).json({ errors: errors.array() });
+  if (!errors.isEmpty()) {
+    logger.error('Validation errors in update request', errors.array())
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  try {
+    // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
+    const email = req.headers.email
+    if (!email) {
+      logger.error('Email not found in the request headers')
+      return res.status(400).json({ error: 'Email not found in the request headers' })
     }
-    
-    try {
-        // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
-        const email = req.headers.email;
-        if (!email) {
-            logger.error('Email not found in the request headers');
-            return res.status(400).json({ error: "Email not found in the request headers" });
-        }
-        // Task 4: Connect to MongoDB
-        const db = await connectToDatabase();
-        const collection = db.collection("users");
-        // Task 5: Find the user credentials in database
-        const existingUser = await collection.findOne({ email });
-        if (!existingUser) {
-            logger.error('User not found');
-            return res.status(404).json({ error: "User not found" });
-        }
-        existingUser.firstName = req.body.name;
-        existingUser.updatedAt = new Date();
-
-        // Task 6: Update the user credentials in the database
-        const updatedUser = await collection.findOneAndUpdate(
-            { email },
-            { $set: existingUser },
-            { returnDocument: 'after' }
-        );
-        // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
-        const payload = {
-            user: {
-                id: updatedUser._id.toString(),
-            },
-        };
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        logger.info('User updated successfully');
-        res.json({ authtoken });
-    } catch (e) {
-        return res.status(500).send('Internal server error');
-
+    // Task 4: Connect to MongoDB
+    const db = await connectToDatabase()
+    const collection = db.collection('users')
+    // Task 5: Find the user credentials in database
+    const existingUser = await collection.findOne({ email })
+    if (!existingUser) {
+      logger.error('User not found')
+      return res.status(404).json({ error: 'User not found' })
     }
-});
+    existingUser.firstName = req.body.name
+    existingUser.updatedAt = new Date()
 
-module.exports = router;
+    // Task 6: Update the user credentials in the database
+    const updatedUser = await collection.findOneAndUpdate(
+      { email },
+      { $set: existingUser },
+      { returnDocument: 'after' }
+    )
+    // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
+    const payload = {
+      user: {
+        id: updatedUser._id.toString()
+      }
+    }
+    const authtoken = jwt.sign(payload, JWT_SECRET)
+    logger.info('User updated successfully')
+    res.json({ authtoken })
+  } catch (e) {
+    return res.status(500).send('Internal server error')
+  }
+})
+
+module.exports = router
